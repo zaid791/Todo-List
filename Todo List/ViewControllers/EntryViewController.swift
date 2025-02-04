abd647c2f76a9f6bac795cccf4803d6f6cc37e7a
Mohammed Zaid Shaikh <zaidshaikh791@gmail.com>
2025-02-04 20:53:57 +0530

    Second Commit

--- Todo List/ViewControllers/EntryViewController.swift
+++ Todo List/ViewControllers/EntryViewController.swift
@@ -7,12 +7,44 @@
 
 import UIKit
 
-class EntryViewController: UIViewController {
-
+class EntryViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var field: UITextField!
+    
+    var update: (() -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        field.delegate = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Save",
+            style: .done,
+            target: self,
+            action: #selector(saveTask)
+        )
     }
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool { 
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func saveTask() {
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        let newCount = count + 1
+        
+        UserDefaults().set(newCount, forKey: "count")
+        UserDefaults().set(text, forKey: "task_\(newCount)")
+        
+        update?()
+        
+        navigationController?.popViewController(animated: true)
+    }
 }
--- Todo List/ViewControllers/ViewController.swift
+++ Todo List/ViewControllers/ViewController.swift
@@ -13,10 +13,45 @@ class ViewController: UIViewController {
     var tasks: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "To Do List"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //Setup defaults
+        if UserDefaults().bool(forKey: "setup"){
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+        }
+        //Get All Current Saved Tasks
+        updateTasks()
     }
-
+    
+    func updateTasks(){
+        tasks.removeAll()
+        
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        for x in 0..<count {
+            if let task = UserDefaults.standard.value(forKey: "task_\(x+1)") as? String{
+                tasks.append(task)
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
     @IBAction func didTapAdd(_ sender: UIBarButtonItem) {
+        let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
+        vc.title = "New Task"
+        vc.update = {
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+        }
+        navigationController?.pushViewController(vc, animated: true)
     }
     
 }
